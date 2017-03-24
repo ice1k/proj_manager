5ac45c4d78b401171b5c9e07644480f6dd309b7e
DCjanus <1102035688@qq.com>
2017-03-25 05:48:17 +0800

    修复一下自己脑残导致的bug

--- src/config.rs
+++ src/config.rs
@@ -12,10 +12,10 @@ pub fn parse_config(path: String) -> Option<Config> {
 
     let lines = buf.lines().filter(|x| x.is_ok()).map(|x| x.unwrap());
     for ln in lines {
-        if let Some(split_index) = ln.find(":") {
-            let (name, item) = ln.split_at(split_index);
-            result.set_fileds(name, item);
-        }
+        let temp: Vec<&str> = ln.split(":").collect();
+        let name = temp[0];
+        let item = temp[1];
+        result.set_fileds(name, item);
     }
     Some(result)
 }
